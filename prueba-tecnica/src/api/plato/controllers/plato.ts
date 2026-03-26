ad485b43a9fc094a24e54e1501e8b94727b95ca5
maroceanix <marmoralespalop@gmail.com>
2026-03-26 17:31:12 +0100

    fix: Tabualation fixed from 2 to 4.

--- prueba-tecnica/src/api/plato/controllers/plato.ts
+++ prueba-tecnica/src/api/plato/controllers/plato.ts
@@ -5,10 +5,10 @@
 import { factories } from "@strapi/strapi";
 
 export default factories.createCoreController("api::plato.plato", ({ strapi }) => ({
-        async obtenerPlatosPopulares(ctx) {
-                const resultado = await strapi
-                        .service("api::plato.plato")
-                        .obtenerPlatosPopulares();
-                ctx.send(resultado);
-        },
+    async obtenerPlatosPopulares(ctx) {
+        const resultado = await strapi
+            .service("api::plato.plato")
+            .obtenerPlatosPopulares();
+        ctx.send(resultado);
+    },
 }));
--- prueba-tecnica/src/api/plato/services/plato.ts
+++ prueba-tecnica/src/api/plato/services/plato.ts
@@ -5,32 +5,30 @@
 import { factories } from "@strapi/strapi";
 
 export default factories.createCoreService("api::plato.plato", ({ strapi }) => ({
-        async obtenerPlatosPopulares() {
-                const menus = await strapi
-                        .documents("api::menu-diario.menu-diario")
-                        .findMany({
-                                populate: ["primero", "segundo", "postre"],
-                        });
+    async obtenerPlatosPopulares() {
+        const menus = await strapi.documents("api::menu-diario.menu-diario").findMany({
+            populate: ["primero", "segundo", "postre"],
+        });
 
-                const conteo = {};
-                for (const menu of menus) {
-                        const platos = [menu.primero, menu.segundo, menu.postre];
-                        for (const plato of platos) {
-                                if (plato) {
-                                        conteo[plato.id] = (conteo[plato.id] || 0) + 1;
-                                }
-                        }
+        const conteo = {};
+        for (const menu of menus) {
+            const platos = [menu.primero, menu.segundo, menu.postre];
+            for (const plato of platos) {
+                if (plato) {
+                    conteo[plato.id] = (conteo[plato.id] || 0) + 1;
                 }
-                const platosOrdenados = Object.entries(conteo).sort(
-                        ([, a], [, b]) => (b as number) - (a as number),
-                );
+            }
+        }
+        const platosOrdenados = Object.entries(conteo).sort(
+            ([, a], [, b]) => (b as number) - (a as number),
+        );
 
-                const [idMasPopular] = platosOrdenados[0]; //coge el primer elemento (el más popular)
+        const [idMasPopular] = platosOrdenados[0]; //coge el primer elemento (el más popular)
 
-                const plato = await strapi.db.query("api::plato.plato").findOne({
-                        where: { id: parseInt(idMasPopular) },
-                });
+        const plato = await strapi.db.query("api::plato.plato").findOne({
+            where: { id: parseInt(idMasPopular) },
+        });
 
-                return { nombre: plato.nombre, cantidad: platosOrdenados[0][1] };
-        },
+        return { nombre: plato.nombre, cantidad: platosOrdenados[0][1] };
+    },
 }));
